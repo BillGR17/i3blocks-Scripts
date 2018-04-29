485e0589af150835910887137e776fa8b29f04a1
Bill Dallas <bill_d17@hotmail.com>
2018-04-29 15:45:10 +0300

    changed bandwidth script moved format function in liblary folder

---
+++ scripts/lib/format.js
@@ -0,0 +1,6 @@
+module.exports=(bytes,d)=> {
+  if(bytes < 1024) return bytes + " b";
+  else if(bytes < 1048576) return(bytes / 1024).toFixed(0) + " kb";
+  else if(bytes < 1073741824) return(bytes / 1048576).toFixed(2) + " mb";
+  else return(bytes / 1073741824).toFixed(2) + " gb";
+}
